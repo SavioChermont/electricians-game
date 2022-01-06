53e6232d3e104270c1895a16384eafd0cc3cbefd
SavioChermont <saviochwt@gmail.com>
2022-01-05 21:51:33 -0300

    feat: init project and create piece

---
+++ ElectriciansGame/src/main/java/br/ufjf/dcc/dcc025/electriciansgame/Game.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufjf.dcc.dcc025.electriciansgame;
+
+/**
+ *
+ * @author savio
+ */
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Game {
+    //Main
+    public static void main(String args[]) {
+        
+        PieceA peca = new PieceA(1);
+        peca.setPieceAPosition(1);
+    }
+}
